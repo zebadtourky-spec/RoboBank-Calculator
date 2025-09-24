b2f14b7bd7cb94ff3d902d52b57233b1ff0eeb81
zebadtourky-spec <zebadtourky@gmail.com>
2025-09-25 00:36:13 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Bank.h"
+using namespace std;
+int main()
+{ int Choice;
+ CreateAccount();
+ cout<<"Enter your Choice"<<endl;
+ cout<<"1:deposit\n";
+ cout <<"2:withdraw\n";
+ cout<<"3:Exit\n";
+ cin>>Choice;
+ if (Choice==1){
+    deposit();
+ }
+ else if (Choice==2){
+    withdraw();
+ }
+ else if (Choice==3){
+   cout<<"Exiting program";
+ }
+ else {
+    cout <<"invalid choice"<<endl;
+ }
+    return 0;
+}
