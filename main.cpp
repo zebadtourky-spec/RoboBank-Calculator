52ef72f326f7147aeaa6de89be409395c4fe76a7
zebadtourky <zebadtourky@gmail.com>
2025-09-25 12:41:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "Bank.h"
+#include "source.h"
+#include <string>
 using namespace std;
 int main()
 { int Choice;
---
+++ source.h
@@ -0,0 +1,11 @@
+#ifndef SOURCE_H
+#define SOURCE_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+void CreateAccount();
+void deposit();
+void withdraw();
+
+#endif
